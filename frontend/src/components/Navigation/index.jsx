854023f2c4c8229d1b95760a16a6dd44f6c75e1d
ElmurodAzodov <elmurodazodov002@gmail.com>
2023-11-22 18:09:26 +0500

    upper-nav

--- frontend/src/components/Navigation/index.jsx
+++ frontend/src/components/Navigation/index.jsx
@@ -1,18 +1,27 @@
 import { Outlet, Link } from "react-router-dom";
 import styles from './style.scss';
+import emailVector from '../../assets/icons/email-vector.png';
+import phoneVector from '../../assets/icons/phone-vector.png'
 
 export default function Navigation() {
     return (
         <main style={styles}>
-            <div className="main-navbar" style={styles}>
-
+            <nav className="main-navbar" style={styles}>
                 <div className="upper-nav">
-
+                    <div className="left-nav">
+                        <a href="mailto:elmurodazodov777@gmail.com">
+                            <img src={emailVector} width="15" height="15" />
+                            elmurodazodov777@gmail.com
+                        </a>
+                        <span>
+                            <img src={phoneVector} width="15" height="15" />
+                            (+998) 917026510
+                        </span>
+                    </div>
+                    <div className="right-nav"></div>
                 </div>
-
-                <Outlet />
-            </div>
+            </nav>
+            <Outlet />
         </main>
-
     )
 }
