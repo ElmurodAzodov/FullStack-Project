9af6f5ec5b853f2639df7ebe8fdbccbe30076e8e
ElmurodAzodov <elmurodazodov002@gmail.com>
2023-12-04 16:28:18 +0500

    created Profile modal

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -41,8 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     'furniture',
+    'users',
     'rest_framework',
     'corsheaders',
+    'rest_framework.authtoken',
     "rest_framework_simplejwt",
 ]
 
